70135a943e2118fc8c0d7ab984c335c304d7852c
Corbin Simpson <cds@corbinsimpson.com>
2013-11-29 20:20:15 -0800

    Add basic setup machinery.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="betterpath",
+    packages=["bp"],
+    setup_requires=["vcversioner"],
+    vcversioner={},
+    install_requires=open("requirements.txt").read().split("\n"),
+    author="Corbin Simpson",
+    author_email="cds@corbinsimpson.com",
+    description="Path manipulation library",
+    long_description=open("README.rst").read(),
+    license="MIT/X11",
+    url="http://github.com/MostAwesomeDude/betterpath",
+)
